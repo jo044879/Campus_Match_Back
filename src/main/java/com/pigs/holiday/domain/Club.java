0c583703feeab3bccb696bbbb96c5d8d2ad96915
gwang <ahngwangeun@gmail.com>
2026-02-07 10:19:57 +0900

    gwang_feat: domain

--- src/main/java/com/pigs/holiday/domain/Club.java
+++ src/main/java/com/pigs/holiday/domain/Club.java
@@ -2,10 +2,14 @@ package com.pigs.holiday.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import com.pigs.holiday.dto.ClubDto;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,17 +22,55 @@ public class Club extends AuditingFields {
     String university;
     String phone;
     String email;
+    String clubName;
+    String description;
+    String region;
+    String sportCategory;
+    int totalMatches;
+    int totalWins;
+    int totalDraws;
+    int totalLosses;
+    int mannerScore;
+
+    @OneToMany(mappedBy = "homeClub")
+    private List<MatchHistory> homeMatchHistoryList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "awayClub")
+    private List<MatchHistory> awayMatchHistoryList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "club")
+    private List<Gallery> galleryList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "club")
+    private List<Schedule> scheduleList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "homeClub")
+    private List<MatchPost> homeMatchPostList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "awayClub")
+    private List<MatchPost> awayMatchPostList = new ArrayList<>();
 
     protected Club(){}
-    private Club(String username, String password, String name, String university, String phone, String email) {
+    private Club(String username, String password, String name, String university, String phone, String email, String clubName, String description, String region, String sportCategory) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.university = university;
         this.phone = phone;
         this.email = email;
+        this.clubName = clubName;
+        this.description = description;
+        this.region = region;
+        this.sportCategory = sportCategory;
+        this.totalMatches = 0;
+        this.totalWins = 0;
+        this.totalDraws = 0;
+        this.totalLosses = 0;
+        this.mannerScore = 80;
     }
-    public static Club of(String username, String password, String name, String university, String phone, String email) { return new Club(username, password,  name, university, phone, email); }
+    public static Club of(String username, String password, String name, String university, String phone, String email, String clubName, String description, String region, String sportCategory) {
+        return new Club(username, password,  name, university, phone, email, clubName, description, region, sportCategory);
+    }
 
     public ClubDto.SignupResDto toSignupResDto() { return ClubDto.SignupResDto.builder().id(getId()).build(); }
 }
---
+++ src/main/java/com/pigs/holiday/domain/Gallery.java
@@ -0,0 +1,36 @@
+package com.pigs.holiday.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Gallery extends AuditingFields {
+    LocalDate matchDate;
+    String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
+
+    @OneToMany(mappedBy = "gallery")
+    private List<GalleryImage> galleryImageList = new ArrayList<>();
+
+    protected Gallery(){}
+    private Gallery(LocalDate matchDate, String name, Club club) {
+        this.matchDate = matchDate;
+        this.name = name;
+        this.club = club;
+    }
+    public static Gallery of(LocalDate matchDate, String name, Club club) {
+        return new Gallery(matchDate, name, club);
+    }
+}

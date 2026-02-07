ef7eae88edac65dc633c308680460a4ae2630ebb
CHO, NAEUN <jo044879@gmail.com>
2026-02-07 19:17:45 +0900

    Fix modify/delete conflict in MatchRequestDto

---
+++ src/main/java/com/pigs/holiday/dto/MatchRequestDto.java
@@ -0,0 +1,81 @@
+package com.pigs.holiday.dto;
+
+import com.pigs.holiday.domain.MatchRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+public class MatchRequestDto {
+
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateReqDto extends DefaultDto.BaseDto {
+        String message;
+        String status;
+
+    }
+
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateResDto extends DefaultDto.BaseDto{
+        Long id;
+    }
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ListResponseDto {
+        Long id;
+        String message;
+        String status;
+    }
+
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateRequestDto extends DefaultDto.BaseDto{
+        Long id;
+        String message;
+        String status;
+    }
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateSevDto extends UpdateRequestDto {
+        Long reqUserId;
+    }
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DeleteReqDto extends DefaultDto.BaseDto {
+        Long id;
+    }
+
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DeleteSevDto extends DeleteReqDto {
+        Long reqUserId;
+    }
+}
+
+
+
+

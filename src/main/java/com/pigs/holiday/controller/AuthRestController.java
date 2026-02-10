96d2309978a1b0ed9f2b5b4eeb23f46e294481ad
gwang <ahngwangeun@gmail.com>
2026-02-10 15:47:15 +0900

    gwang_feat: SecurityConfig.java

--- src/main/java/com/pigs/holiday/controller/AuthRestController.java
+++ src/main/java/com/pigs/holiday/controller/AuthRestController.java
@@ -10,10 +10,7 @@ import com.pigs.holiday.security.PrincipalDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
